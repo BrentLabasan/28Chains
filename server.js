e9e789b91e057909edc1e2eeaa434a752a5680fd
BrentLabasan <brentlabasan@gmail.com>
2016-02-11 11:09:03 -0800

    Building Angular and Node.js Apps with the MEAN Stack
    shieeeeeettt.....had to modify the server.js to use the static file directories on like 6 and 7....now I can access index.html, Angular's all hooked up and activated, AND THE WEB-ENGINEERING.INFO PROJECT WORKS
    
    now time to built my app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+app.use(express.static(__dirname + ''));
 app.use(express.static(__dirname + '/partials'));
 
 var port = 3030;

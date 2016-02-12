a05e8d5b60b72bbaa942ab69be66fddc664a4459
BrentLabasan <brentlabasan@gmail.com>
2016-02-11 21:23:33 -0800

    configing

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 app.use(express.static(__dirname + ''));
 app.use(express.static(__dirname + '/partials'));
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.static('src'));
 
 var port = process.env.PORT || CONFIG.port || 3030;
 app.listen(port);

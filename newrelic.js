c11e0e827c86b6f8241ef08b71d97634572fb228
BrentLabasan <brentlabasan@gmail.com>
2016-02-13 22:52:43 -0800

    added NewRelic files

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['28Chains'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '1bab99384e64adfe30165882249a75a833a7d6e7',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}

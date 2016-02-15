21d54f830b940d55dd122d1cca4f366eeea13f0e
BrentLabasan <brentlabasan@gmail.com>
2016-02-14 21:05:00 -0800

    weekend days in date picker on createHabit are now selectable
    NEXT UP: Get authorization of read and write rules on Firebase working.

--- js/pagination.js
+++ js/pagination.js
@@ -30,9 +30,9 @@ angular.module('ui.bootstrap.demo')
     };
 
     // Disable weekend selection
-    $scope.disabled = function(date, mode) {
+/*    $scope.disabled = function(date, mode) {
         return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-    };
+    };*/
 
     $scope.toggleMin = function() {
         $scope.minDate = $scope.minDate ? null : new Date();

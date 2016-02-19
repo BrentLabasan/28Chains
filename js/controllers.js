f5107a2d55c42dc5e8a670ddc694aee17a9bc15d
BrentLabasan <brentlabasan@gmail.com>
2016-02-19 10:56:37 -0800

    changed title
    TODO however, the Habit Name field under an Attempt is not updated
    
    NEXT UP: Get authorization of read and write rules on Firebase working.

--- js/controllers.js
+++ js/controllers.js
@@ -78,10 +78,10 @@ publicLibraryControllers.controller('BooksController',
 
             $scope.woof = "woof1";
             $scope.id_habit = $routeParams.id;
-            var ref3 = new Firebase("https://glowing-heat-6414.firebaseio.com/attempts/" + $routeParams.id)
+            var ref3 = new Firebase("https://glowing-heat-6414.firebaseio.com/attempts/" + $routeParams.id);
                 //.equalTo($routeParams.id, "id_habit")
                 //.equalTo($routeParams.id)
-                ;
+
             var syncobject = $firebaseObject(ref3);
             syncobject.$bindTo($scope, "data");
             console.log("syncobject " + syncobject);

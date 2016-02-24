ba5281361ff684464fedfc42427d1a2662535b0a
BrentLabasan <brentlabasan@gmail.com>
2016-02-23 16:30:03 -0800

    removed lots of comments from controllers.js

--- app/engine28/controllers.js
+++ app/engine28/controllers.js
@@ -6,15 +6,7 @@
   var twentyEightChainsControllers = angular.module('twentyEightChainsControllers',
     ['firebase', 'ui.bootstrap.demo', 'ngSanitize']);
 
-  /*******************************************************************************
-   *******************************************************************************
-   * # CoreController:
-   * - for showAllBooks, addHabit, updateHabit and destroyHabit.
-   *
-   * @param  {Object} $scope      [glue between view/model and controller]
-   * @param  {Object} $location   [control the url]
-   * @param  {Object} $http       [get connection with parse.com using REST Api]
-   ******************************************************************************/
+
   twentyEightChainsControllers.controller('CoreController',
     ['$scope', '$location', '$http', '$firebaseObject', '$firebaseArray', '$firebaseAuth', 'Auth', '$routeParams',
       function ($scope, $location, $http, $firebaseObject, $firebaseArray, $firebaseAuth, Auth, $routeParams) {
@@ -48,11 +40,7 @@
         });
 
 
-        /**
-         * ## showLoading
-         * once the page is loaded, only show progressbar, which will be hidden after
-         * loaded all data, and the rest content of this page will be shown;
-         */
+
         $scope.showLoading = true;
 
         $scope.getAttemptData = function () {
@@ -136,12 +124,7 @@
          //return attemptRef.startDate;
          };*/
 
-        /**
-         * ## getAllHabits
-         * sent a GET request to parse.com once the controller is loaded. If the
-         * request is succeed, all books stored in parse.com will be loaded and
-         * saved to an array $scope.books; otherwise, show error messages in console.
-         */
+
         $scope.getAllHabits = function () {
           console.log("begin getAllHabits()");
 
@@ -159,13 +142,6 @@
         };
 
 
-        /**
-         * ## addHabit
-         * send a POST request to parse.com for inserting a new book record which
-         * stored in $scope.book to parse.com. If the request is succeed, clear all
-         * the temporary data and jump to parent page; otherwise, show error messages
-         * in console.
-         */
 
         $scope.addHabit = function () {
           // create the Habit
@@ -228,13 +204,7 @@
 
         };
 
-        /**
-         * ## updateHabit
-         * send a PUT request to parse.com for changing book record, which has a
-         * standard objectId defined by parse.com itself and stored in $scope.book,
-         * to parse.com. If the request is succeed, clear all the temporary data and
-         * jump to parent page; otherwise, show error messages in console.
-         */
+
         $scope.updateHabit = function () {
 
           var url = "https://glowing-heat-6414.firebaseio.com/attempts/" + $scope.book.id + "/" + $scope.book.id_attempt;
@@ -279,12 +249,7 @@
 
         };
 
-        /**
-         * ## destroyHabit
-         * send a DELETE request to parse.com for deleting a book record which has
-         * a standard objectId defined by parse.com itself. If the request is succeed,
-         * jump to parent page; otherwise, show error messages in console.
-         */
+
         $scope.destroyHabit = function () {
           // gets the reference to the Habit to delete
           var habitToDelete = reference_FirebaseRoot.child("habits/" + $scope.authData.uid + "/" + $scope.book.id);
@@ -312,101 +277,6 @@
 
       }]);
 
-
-  /*==============================================================================
-   *==============================================================================
-   * # TestDatasController
-   * - for creating and clearing test data.
-   ******************************************************************************/
-  twentyEightChainsControllers.controller('TestDatasController',
-    ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
-      // Create test data.
-      $scope.createTestData = function () {
-        $scope.clearDatabase();
-        $timeout(function () {
-          // basic data from author, publisher and book
-          $http({
-            method: 'POST',
-            url: 'https://api.parse.com/1/batch',
-            data: {
-              requests: [
-                // Create Test Data for Books
-                {
-                  method: 'POST',
-                  path: '/1/classes/Book',
-                  body: {
-                    isbn: "0553345842",
-                    title: "The Mind's I",
-                    year: 1982
-                  }
-                },
-                {
-                  method: 'POST',
-                  path: '/1/classes/Book',
-                  body: {
-                    isbn: "1463794762",
-                    title: "The Critique of Pure Reason",
-                    year: 2011
-                  }
-                },
-                {
-                  method: 'POST',
-                  path: '/1/classes/Book',
-                  body: {
-                    isbn: "1928565379",
-                    title: "The Critique of Practical Reason",
-                    year: 2009
-                  }
-                },
-                {
-                  method: 'POST',
-                  path: '/1/classes/Book',
-                  body: {
-                    isbn: "0465030793",
-                    title: "I Am A Strange Loop",
-                    year: 2000
-                  }
-                }
-              ]
-            }
-          })
-            .success(function () {
-              console.log("added 4 books");
-            })
-            .error(function (data) {
-              console.log(data);
-              alert("OH! Something goes wrong. See the information in console.");
-            });
-        }, 2000);
-      };
-
-      // Clear all test data.
-      $scope.clearDatabase = function () {
-        // Clear all Book Data
-        $http({
-          method: 'GET',
-          url: 'https://api.parse.com/1/classes/Book'
-        })
-          .success(function (data) {
-            var obs = data.results;
-            obs.forEach(function (ob) {
-              $http({
-                method: 'DELETE',
-                url: 'https://api.parse.com/1/classes/Book/' + ob.objectId
-              })
-                .error(function (data, status, headers, config) {
-                  alert("OH! Something goes wrong. See the information in console.");
-                  console.log(data, status, headers, config);
-                });
-            });
-            console.log("clear all book data.");
-          })
-          .error(function (data, status, headers, config) {
-            alert("OH! Something goes wrong. See the information in console.");
-            console.log(data, status, headers, config);
-          });
-      };
-    }]);
 
 
 }());

bec9fad5f6ee5ed2407c3959a2963fe8a2c4437c
BrentLabasan <brentlabasan@gmail.com>
2016-03-01 10:00:41 -0800

    attempting to add attemptViewer directive TO FIX enter a valid format (blank text has been taken care of) or else it will mess up

---
+++ app/attemptViewer/attemptViewer.directive.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.attemptViewer', [])
+    .directive("w3TestDirective", function () {
+      return {
+        restrict: "E",
+        template: "<h1>Made by a directive!</h1>"
+      };
+
+    })();
+
+
+});

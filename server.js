d19799f75865243576113338f29aeddc87e2d6ba
BrentLabasan <brentlabasan@gmail.com>
2016-03-04 21:30:24 -0800

    SUCCESS if I change FB/Habits, node.js logs it! awesome!

--- server.js
+++ server.js
@@ -12,6 +12,41 @@ app.use(express.static('src'));
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
+
+
+var Firebase = require('firebase');
+/*var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});*/
+
+// listen for changes to Firebase data
+var fb = new Firebase('https://glowing-heat-6414.firebaseio.com/habits/');
+fb.on('child_added',   function(oldChildSnapshot) {
+  // code to handle child removal.
+  console.log("a")
+});
+fb.on('child_changed', createOrUpdateIndex);
+fb.on('child_removed', removeIndex);
+
+function createOrUpdateIndex() {
+  console.log('ayyyyy');
+/*
+  client.index(this.index, this.type, snap.val(), snap.key())
+    .on('data', function(data) { console.log('indexed ', snap.key()); })
+    .on('error', function(err) { /!* handle errors *!/ });
+*/
+}
+function removeIndex() {
+  console.log('lmao');
+
+  /*  client.deleteDocument(this.index, this.type, snap.key(), function(error, data) {
+      if( error ) console.error('failed to delete', snap.key(), error);
+      else console.log('deleted', snap.key());
+    });*/
+}
+
 var port = process.env.PORT || 3030;
 app.listen(port);
 console.log("Listening on port " + port);

fa8486db1a262875dd4237d0820872f76a91ac59
BrentLabasan <brentlabasan@gmail.com>
2016-03-06 14:02:36 -0800

    CHECKPOINT gonna ove everything from ModalDemoCtrl and ModalInstanceCtrl to attemptViewer.controller.js

--- app/angularBootstrap/pagination.js
+++ app/angularBootstrap/pagination.js
@@ -159,8 +159,41 @@ angular.module('angularBootstrap')
       $uibModalInstance.dismiss('cancel');
     };
 
-    $scope.loadAtmptFrmSrch = function() {
+    $scope.setAttemptLoaded = function(atmptId) {
+
+      if (atmptId) {
+        // retrieves text in
+        $scope.attemptIdLoaded = document.getElementById("queryAttemptID").value;
+        //console.log("setting Attempt loaded to ID " + $scope.attemptIdLoaded);
+        var arr = $scope.attemptIdLoaded.split("/");
+        var idHabit = arr[0];
+        var idAttempt = arr[1];
+        console.log(idHabit + " " + idAttempt);
+
+        // TODO in progress
+        //var url_attempt = reference_FirebaseRoot + "attempts/" + $routeParams.idhabit + "/" + $routeParams.idattempt;
+        var url_attempt = reference_FirebaseRoot + "attempts/" + idHabit + "/" + idAttempt;
+        var refFbase_attempt = new Firebase(url_attempt);
+        var syncObjectAttempt = $firebaseObject(refFbase_attempt);
+        syncObjectAttempt.$bindTo($scope, "otherAttempt");
+        var xxx = refFbase_attempt.child('uid');
+        var userId;
+        refFbase_attempt.child('uid').on("value", function(snapshot) {
+          userId = snapshot.val();
+          //var url_Habit = reference_FirebaseRoot + "habits/" + currentAuth.uid + "/" + $routeParams.idhabit;
+          var url_Habit = reference_FirebaseRoot + "habits/" + userId + "/" + idHabit;
+          var refFbase_habit = new Firebase(url_Habit);
+          var syncObjectHabit = $firebaseObject(refFbase_habit);
+          syncObjectHabit.$bindTo($scope, "otherHabit");
+        });
+      }
+
+    };
+
+    $scope.loadAtmptFrmSrch = function(atmptId) {
       //alert();
+      $scope.setAttemptLoaded(atmptId);
+      $scope.cancel();
     };
 
     $scope.searchForAttempts = function(term) {
